03622a4a4208ee9b340e6cb0753c45c60288666c
Shubh Goyal <goyal.shubh0711@gmail.com>
2026-07-07 16:51:07 +0530

    Step 18: scale_attention_scores

--- model.py
+++ model.py
@@ -156,8 +156,12 @@ def compute_raw_attention_scores(query, key):
     key_dim = key.dim()
     return query @ key.transpose(key_dim-1, key_dim-2)
 
-# Step 18 - scale_attention_scores (not yet solved)
-# TODO: implement
+# Step 18 - scale_attention_scores
+import torch
+import math
+
+def scale_attention_scores(scores, d_k):
+    return scores / (d_k**(1/2))
 
 # Step 19 - mask_attention_scores_with_neg_inf (not yet solved)
 # TODO: implement
