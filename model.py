8640a64b4aaea09fc9fdef559a6fd0f3a1c44661
Shubh Goyal <goyal.shubh0711@gmail.com>
2026-07-08 18:45:45 +0530

    Step 65: update_adam_first_moment

--- model.py
+++ model.py
@@ -700,8 +700,13 @@ def initialize_adam_optimizer_state(parameter_list):
     
     return optim_dict
 
-# Step 65 - update_adam_first_moment (not yet solved)
-# TODO: implement
+# Step 65 - update_adam_first_moment
+import torch
+
+def update_adam_first_moment(m_prev, grad, beta1):
+    """Return m_t = beta1 * m_prev + (1 - beta1) * grad."""
+    m_t = beta1 * m_prev + (1 - beta1) * grad
+    return m_t
 
 # Step 66 - update_adam_second_moment (not yet solved)
 # TODO: implement
