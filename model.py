e74e7c29bec0879ee3e29ab8eb58749692832e1c
Shubh Goyal <goyal.shubh0711@gmail.com>
2026-07-08 11:13:31 +0530

    Step 42: stack_encoder_layers

--- model.py
+++ model.py
@@ -361,8 +361,12 @@ def assemble_encoder_layer(x, layer_params, num_heads, src_mask):
     
     return o
 
-# Step 42 - stack_encoder_layers (not yet solved)
-# TODO: implement
+# Step 42 - stack_encoder_layers
+def stack_encoder_layers(x, encoder_layer_params_list, num_heads, src_mask):
+    for i, layer_params in enumerate(encoder_layer_params_list):
+        x = assemble_encoder_layer(x, layer_params, num_heads, src_mask)
+    
+    return x
 
 # Step 43 - decoder_layer_masked_self_attention_sublayer (not yet solved)
 # TODO: implement
